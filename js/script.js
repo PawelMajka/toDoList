b1e293c1114b10a452cde3560e416c1f2999d5d2
Pawel Majka <pawelmajka1989@gmail.com>
2022-12-29 22:17:22 +0100

    added addNewTask and remuveButtons tasks

--- js/script.js
+++ js/script.js
@@ -22,11 +22,43 @@
             <li
                 ${task.done ? "class=\"tasks__content--done\"" : ""} js-task
             >
+            <button class="tasks__button tasks__button--toogleDone js-toggleDone"> ✔ </button>
+            <button class="tasks__button tasks__button--remuve js-remove">🗑</button>
                 ${task.content}
             </li>
             `;
         };
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                tasks.splice(index, 1);
+                render();
+            });
+        });
+    };
+
+    const addNewTask = (newTaskContent) => {
+        
+        tasks.push({
+            content: newTaskContent,  
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            //console.log(newTaskContent);
+            if (newTaskContent === "") {
+                return;
+            };
+
+            addNewTask(newTaskContent);
     };
 
     const init = () => {
@@ -34,22 +66,7 @@
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            //console.log(newTaskContent);
-
-            if (newTaskContent === "") {
-                return;
-            };
-
-            tasks.push({
-                content: newTaskContent,  
-            });
-
-            render();
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init ();
